059529d47321aab275d528236b13c82616cb258a
Daniel Li <danli@lcs.on.ca>
2024-11-20 10:16:57 -0500

    Initial Commit

---
+++ FourSeason/FourSeasonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourSeasonApp.swift
+//  FourSeason
+//
+//  Created by 李泽宇 on 2024-11-20.
+//
+
+import SwiftUI
+
+@main
+struct FourSeasonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
