5eb623e3a26c437e8efe23fcf43123301af7c80c
Daniel Li <danli@lcs.on.ca>
2024-11-20 10:33:41 -0500

    Done the first Spring structure structed

--- FourSeason/ContentView.swift
+++ FourSeason/ContentView.swift
@@ -9,15 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+          
+                VStack{
+                    ZStack{
+                        Rectangle()
+                            .frame (width: 400, height: 150)
+                            .opacity(0.5)
+                        HStack{
+                            Image(systemName: "cloud.rain.circle.fill")
+                                .scaledToFill()
+                            VStack{
+                                Text("Spring")
+                                    .font(.largeTitle)
+                                Text("Spring is beautiful, everuthing grows and blossom.")
+                                    .font((.subheadline))
+                            }
+                        }
+                    }
+                
+                
+                
+                Text("Summer")
+                        Text("Summer is hot, everything feels melted.")
+                        Text("Autumn")
+                        Text("Autumn is cool, everything falls, and change colours.")
+                        Text("Winter")
+                        Text("Winter is cold, everything is frozen")
+                        
+                    
+                }
+            }
         }
-        .padding()
     }
-}
 
 #Preview {
     ContentView()
